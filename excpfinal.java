b4d34573b52449b238d5d6ec681f1558ad3cb295
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-02 20:24:47 +0530

    Create excpfinal.java

---
+++ excpfinal.java
@@ -0,0 +1,17 @@
+public class expfinal {
+  public static void main(String[] args) {
+    try 
+	{
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[2]);
+    } 
+	catch (Exception e)
+	{
+      System.out.println("Something went wrong");
+    }
+	finally 
+	{
+      System.out.println("move to next");
+    }
+  }
+}
